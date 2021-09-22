c615d3a0bf6c47f1ea61f336e81fba74c44c3212
Atieh <atieh.rahbari@gmail.com>
2021-09-22 19:49:12 +0330

    modify month delete aban

--- test.java
+++ test.java
@@ -5,7 +5,7 @@ public class Date {
     private int year;
     private int month;
     private final String[] months = {"farvardin", "ordibehesht", "khordad", "tir", "mordad", "shahrivar"
-            , "mehr", "aban", "azar", "dey", "bahman", "esfand"};
+            , "mehr", "azar", "dey", "bahman", "esfand"};
 
     public Date(int year, int month, int day) {
         this.year = year;
